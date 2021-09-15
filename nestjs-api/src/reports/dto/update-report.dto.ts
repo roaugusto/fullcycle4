d440a1192491690525d6f8d98d0a7512b4dc2e30
Rodrigo Santos <ro.augusto@gmail.com>
2021-09-14 22:39:21 -0300

    first commit

---
+++ nestjs-api/src/reports/dto/update-report.dto.ts
@@ -0,0 +1,15 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsIn, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { ReportStatus, ReportStatusList } from '../entities/report.entity';
+import { CreateReportDto } from './create-report.dto';
+
+export class UpdateReportDto extends PartialType(CreateReportDto) {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  file_url: string;
+
+  @IsIn(ReportStatusList)
+  @IsNotEmpty()
+  status: ReportStatus;
+}
